6c35e85a2e3796b15583578540f2761a3117eff4
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2023-01-07 22:13:35 -0300

    refactoring

--- script1.js
+++ script1.js
@@ -1,4 +1,4 @@
-(() => {
+;(() => {
   const ul = document.querySelector("[data-js='pokedex']")
   const fetchPokemon = () => {
     const pokemonsNumbers = 905
@@ -9,27 +9,67 @@
       pokemonPromises.push(fetch(getPokemonUrl(i)).then(r => r.json()))
     }
 
-    Promise.all(pokemonPromises).then(pokemons => {
-      const lisPokemons = pokemons.reduce((acc, pokemon) => {
-        const types = pokemon.types.map(typeInfo => typeInfo.type.name)
+    Promise.all(pokemonPromises)
+      .then(pokemons => {
+        const lisPokemons = pokemons.reduce((acc, pokemon) => {
+          const types = pokemon.types.map(typeInfo => typeInfo.type.name)
 
-        acc += `
+          acc += `
       <li class="card ${types[0]}" >
       <img class="card-image " alt="${pokemon.name}" src="${
-          pokemon['sprites']['other']['official-artwork']['front_default']
-        }" />
+            pokemon['sprites']['other']['official-artwork']['front_default']
+          }" />
       <h2 class="card-title">${pokemon.id.toString().padStart(3, '0')}. ${
-          pokemon.name
-        }</h2>
+            pokemon.name
+          }</h2>
       <p class="card-subtitle">${types.join(' | ')}</p>
       </li>`
 
-        return acc
-      }, '')
+          return acc
+        }, '')
 
-      ul.innerHTML = lisPokemons
-    })
+        ul.innerHTML = lisPokemons
+        filter()
+      })
+      
   }
 
   fetchPokemon()
+
+  function filter() {
+    console.log("teste")
+    const Load = document.querySelector('#load').style.display = 'none'
+
+
+    const filterElement = document.querySelector('header input')
+
+    const cards = document.querySelectorAll('.pokedex li ')
+
+    filterElement.addEventListener('input', () => {
+      filterCards()
+    })
+
+    function filterCards() {
+      if (filterElement.value !== '') {
+        for (let card of cards) {
+          let pokemonName = `${card.querySelector('h2').textContent} ${
+            card.querySelector('p').textContent
+          }`
+
+          pokemonName = pokemonName.toLocaleLowerCase()
+
+          let filterText = filterElement.value.toLocaleLowerCase()
+          if (!pokemonName.includes(filterText)) {
+            card.style.display = 'none'
+          } else {
+            card.style.display = 'block'
+          }
+        }
+      } else {
+        for (let card of cards) {
+          card.style.display = 'block'
+        }
+      }
+    }
+  }
 })()
